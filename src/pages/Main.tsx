27d75d00dc0a1d0b760f923edafbc4dc8477df5e
AlexKjes <alexander.kjeserud@nav.no>
2024-09-25 10:27:25 +0200

    Tveaker style for hovedmenyknapper og endrer fav icon og tittel

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,20 +4,18 @@ export const Main = () => {
   return (
     <nav className="flex justify-center items-center min-h-screen bg-gray-100">
       <ul className="flex flex-col md:flex-row gap-4">
-        <li>
-          <Link
-            to="/which-principle"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300"
-          >
-            Which Agile Principle Are You?
+        <li className="basis-1/2">
+          <Link to="/which-principle">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32">
+              Which Agile Principle Are You?
+            </button>
           </Link>
         </li>
-        <li>
-          <Link
-            to="/how-agile-is-your-team"
-            className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300"
-          >
-            How Agile Is Your Team?
+        <li className="basis-1/2">
+          <Link to="/how-agile-is-your-team">
+            <button className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32 w-full">
+              How Agile Is Your Team?
+            </button>
           </Link>
         </li>
       </ul>

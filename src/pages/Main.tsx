35057596887ea11400f29424a9486f7cedf8362a
AlexKjes <alexander.kjeserud@nav.no>
2024-09-28 14:11:28 +0200

    Changes main page button colors to match RL colors

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,14 +6,14 @@ export const Main = () => {
       <ul className="flex flex-col md:flex-row gap-4">
         <li className="basis-1/2">
           <Link to="/which-principle">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32">
+            <button className="bg-black hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32">
               Which Agile Principle Are You?
             </button>
           </Link>
         </li>
         <li className="basis-1/2">
           <Link to="/how-agile-is-your-team">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32 w-full">
+            <button className="bg-red-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-300 h-32 w-full">
               How Agile Is Your Team?
             </button>
           </Link>
